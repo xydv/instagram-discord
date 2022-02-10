c30631bf7c0fcf236e9176dcbb1e8bb89caaabc7
icancodebeauty <adi@mail.io>
2022-02-10 14:06:19 +0000

    Updated!.

--- bot.js
+++ bot.js
@@ -22,20 +22,31 @@ client.on('messageCreate', async (message) => {
             let username = splited[1];
             let url = `https://instapi.deta.dev/${username}`;
             let fetching = await fetch(url);
-            let response = await fetching.json()
-            let IEMBED = new MessageEmbed()
-                .setColor("#e95950")
-                .setTitle(response.name)
-                .setURL(`https://instagram.com/${username}`)
-                .setDescription(response.bio)
-                .addFields(
-                    { name: `Username`, value: `\`${response.username}\`` },
-                    { name: `Posts`, value: `\`${response.posts}\``, inline: true },
-                    { name: `Followers`, value: `\`${response.followers}\``, inline: true },
-                    { name: `Following`, value: `\`${response.following}\``, inline: true },
-                )
-                .setTimestamp(Number(response.timestamp));
-            message.channel.send({ embeds: [IEMBED] });
+            let status = fetching.status;
+            if (!(status >= 400)) {
+                let response = await fetching.json()
+                let IEMBED = new MessageEmbed()
+                    .setColor("#e95950")
+                    .setTitle(response.name)
+                    .setURL(`https://instagram.com/${username}`)
+                    .setDescription(response.bio)
+                    .addFields(
+                        { name: `Username`, value: `\`${response.username}\`` },
+                        { name: `Posts`, value: `\`${response.posts}\``, inline: true },
+                        { name: `Followers`, value: `\`${response.followers}\``, inline: true },
+                        { name: `Following`, value: `\`${response.following}\``, inline: true },
+                    )
+                    .setTimestamp(Number(response.timestamp));
+                message.channel.send({ embeds: [IEMBED] });
+            } else {
+                message.channel.send(
+                    new MessageEmbed()
+                        .setColor("#e95950")
+                        .setTitle(`Error`)
+                        .setDescription("Profile is private or doesn't exist!.")
+                        .setTimestamp()
+                );
+            }
         }
     }
 })
